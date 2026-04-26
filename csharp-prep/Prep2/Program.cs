f8e794410832d8090eef9e980c9da098d66bb36c
Yuki Nishigaki <yukinishigaki@byui.edu>
2026-04-26 16:10:29 +0900

    Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        float percentage = float.Parse(userInput);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            // Console.WriteLine("A");
+            letter = "A";
+        }
+        else if (percentage < 90 && percentage >= 80)
+        {
+            // Console.WriteLine("B");
+            letter = "B";
+        }
+        else if (percentage < 80 && percentage >= 70)
+        {
+            // Console.WriteLine("C");
+            letter = "C";
+        }
+        else if (percentage < 70 && percentage >= 60)
+        {
+            // Console.WriteLine("D");
+            letter = "D";
+        }
+        else
+        {
+            // Console.WriteLine("F");
+            letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying next time!");
+        }
+
     }
 }
